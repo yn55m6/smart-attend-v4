68d55fd38bb013e934326f035eac9b7dbb7002a8
yn55m6 <yn55m6@gmail.com>
2026-02-27 06:59:24 +0000

    Fix: Final infrastructure setup for V4 entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// HTML의 id="root"인 요소에 리액트 앱을 주입합니다.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
